9a9d5c091c6d5db1fea630a09aa07ffd0013d8c0
Sebastian.Valdivia <se.valdiviae@duocuc.cl>
2025-05-25 14:58:28 -0400

    Refactor EnrollmentDTO to simplify imports and enforce @NotNull
    Cleaned up unused imports to improve code readability and maintainability. Added @NotNull annotations for studentId and courseId to ensure proper validation. These changes enhance the clarity and integrity of the EnrollmentDTO class.

--- src/main/java/com/edutech/classroom/dto/EnrollmentDTO.java
+++ src/main/java/com/edutech/classroom/dto/EnrollmentDTO.java
@@ -1,24 +1,21 @@
 package com.edutech.classroom.dto;
 
-
-import com.edutech.classroom.entity.Course;
-import jakarta.persistence.Column;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
-import lombok.Data;
-import org.hibernate.annotations.ColumnDefault;
-
 import java.time.Instant;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 
 @Data
 public class EnrollmentDTO {
 
     private Integer id;
+
+    @NotNull
     private Integer studentId;
+
+    @NotNull
     private Integer courseId;
+
     private Instant enrolledAt;
     private String status;
 
