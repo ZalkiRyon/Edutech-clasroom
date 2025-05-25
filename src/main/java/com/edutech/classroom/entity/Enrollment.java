50979a1df048390c6087c52339695b6740718a75
Ryon00 <se.valdiviae@duocuc.cl>
2025-05-25 01:33:22 -0400

    Add initial project setup with Maven wrapper and entities
    This commit sets up the project structure with a `.gitignore`, `.gitattributes`, Maven Wrapper, and application configuration file. It also includes the definition of core entity classes like `Course`, `CourseCategory`, `CourseContent`, `DiscountCoupon`, and others for the Classroom application.

---
+++ src/main/java/com/edutech/classroom/entity/Enrollment.java
@@ -0,0 +1,41 @@
+package com.edutech.classroom.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "enrollment")
+public class Enrollment {
+    @Id
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "student_id", nullable = false)
+    private com.edutech.classroom.entity.User student;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "course_id", nullable = false)
+    private Course course;
+
+    @NotNull
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    @Column(name = "enrolled_at", nullable = false)
+    private Instant enrolledAt;
+
+    @Size(max = 20)
+    @NotNull
+    @Column(name = "status", nullable = false, length = 20)
+    private String status;
+
+}
---
+++ src/main/java/com/edutech/classroom/entity/Role.java
@@ -0,0 +1,31 @@
+package com.edutech.classroom.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "role")
+public class Role {
+    @Id
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Size(max = 50)
+    @NotNull
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+
+    @Size(max = 800)
+    @NotNull
+    @Column(name = "description", nullable = false, length = 800)
+    private String description;
+
+}
